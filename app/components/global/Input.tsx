d2723aa17994593c6594a52a31e64a4770cc5db9
sebastianarcher0336 <sebastianarcher002@gmail.com>
2025-04-09 22:42:34 +0700

    Revert "fix event click when not submit"
    This reverts commit 949a114de767b2531d47813d3b39a991c4a537de.

--- app/components/global/Input.tsx
+++ app/components/global/Input.tsx
@@ -9,7 +9,6 @@ interface Props {
   placeholder: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void; // Accepts an event parameter
   error?: string;
-  disable: boolean;
 }
 
 const Input = ({
@@ -19,8 +18,7 @@ const Input = ({
   label,
   placeholder,
   onChange,
-  error,
-  disable,
+  error
 }: Props) => {
 
   console.log("Input Props:", {
@@ -30,17 +28,7 @@ const Input = ({
     label,
     placeholder,
     error,
-    disable,
   });
-
-  // Create a debounced version of the onChange handler
-  const handleChange = debounce(async (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (!disable && onChange) {
-      onChange(e);
-    }
-  }, 1000);
-
-
   return (
     <div className={classname}>
       <label htmlFor={name_input || type}>{label}</label>
@@ -49,12 +37,11 @@ const Input = ({
         name={name_input || type} // Use type as default if name_input is not provided
         placeholder={placeholder}
         className="form-control"
-        onChange={handleChange} // Pass the debounced onChange to the input
+        onChange={onChange} // Pass the onChange prop to the input
       />
       {error && <small className="text-danger">{error}</small>} {/* Display error message */}
     </div>
   );
 };
 
-// Export the memoized component
-export default memo(Input);
+export default Input;
